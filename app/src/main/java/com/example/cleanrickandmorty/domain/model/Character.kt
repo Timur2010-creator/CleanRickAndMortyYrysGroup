a37c4a34b1efad7c729917720fb2cf8586da1678
Timur2010-creator <timuraitbaev7@gmail.com>
2026-07-17 04:47:46 +0600

    nothing has changed.

--- app/src/main/java/com/example/cleanrickandmorty/domain/model/Character.kt
+++ app/src/main/java/com/example/cleanrickandmorty/domain/model/Character.kt
@@ -15,6 +15,6 @@ data class Character(
         val species: String,
         val type: String,
         val episode: List<String>,
-        val location: Location
+        val location: Location,
         )
 }
